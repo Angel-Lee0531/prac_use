8e836eae7703b972252a654118f7c78774bc56b0
Angel <angel8200531@gmail.com>
2021-03-10 16:39:45 -0800

    3/11

--- text/models.py
+++ text/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     content = models.CharField(max_length=100)
+    sub_date = models.DateTimeField(default = timezone.now)
--- text/views.py
+++ text/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
     return render(request, 'home.html', {'hello':'you can enter some words'})
